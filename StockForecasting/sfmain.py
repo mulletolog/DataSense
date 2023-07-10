c6370cd057422dbd0fa2fb6ac4639f5302254671
mulletolog <narek.tahmazyan1@gmail.com>
2023-07-09 18:33:27 -0700

    upld

---
+++ StockForecasting/sfmain.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from datetime import date
+
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+START = '2013-01-01'
+TODAY = date.today().strftime('%Y-%m-%d')
+
+st.title('Stock Prediction App')
+st.write('This app predicts the stock prices of selected companies using Facebook Prophet.')
+
+stocks = ('TM', 'HMC', 'GM', 'F', 'STLA', 'RACE')
+selected_stock = st.selectbox('Select Dataset for Predictions', stocks)
+
+n_years = st.slider('Years of Predictions:', 1, 10)
+period = n_years * 365
+
+@st.cache_data
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text('Load data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open', line=dict(color='lightblue')))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close', line=dict(color='lightcoral')))
+    fig.layout.update(title_text='Time Series Data', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
+
+#Froecasting
+df_train = date[['Date', 'Close']]
+df_train = df_train.rename(columns={'Date':'ds', 'Close':'y'})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dateframe(periods=period)
+forecast = m.predict(future)
+
+st.subheader('Forecast data')
+st.write(forecast.tail())
+
+st.write('forecast data')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write('forecast components')
+fig2 = m.plot_components(forecast)
+st.write(fig2)
